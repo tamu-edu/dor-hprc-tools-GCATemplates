452ed26c200a2be3046026d1ba61e76f5e1c6dea
Charles Michael Dickens <cmdickens@llogin2.launch>
2025-09-18 11:19:09 -0500

    adding freebayes

---
+++ templates/launch/run_freebayes_1.3.7_launch.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --job-name=freebayes        # job name
+#SBATCH --time=1-00:00:00           # max job run time dd-hh:mm:ss
+#SBATCH --ntasks-per-node=1         # tasks (commands) per compute node
+#SBATCH --cpus-per-task=48          # CPUs (threads) per command
+#SBATCH --mem=92G                   # total memory per node
+#SBATCH --output=stdout.%x.%j       # save stdout to file
+#SBATCH --error=stderr.%x.%j        # save stderr to file
+
+module purge
+module load GCC/12.3.0 freebayes/1.3.7-R-4.3.2
+
+<<README
+    - FreeBayes manual: https://github.com/ekg/freebayes
+README
+
+################################### VARIABLES ##################################
+# TODO Edit these variables as needed:
+
+########## INPUTS ##########
+reference_fasta='/scratch/data/bio/GCATemplates/data/e_coli/ref/GCF_000005845.2_ASM584v2_genomic.fna'
+alignments_bam='/scratch/data/bio/GCATemplates/data/e_coli/bam/SRR10561103_sorted.bam'
+
+######## PARAMETERS ########
+sample_name='SRR10561103'
+ploidy=1
+
+########## OUTPUTS #########
+output_vcf="${sample_name}_freebayes_out.vcf"
+
+################################### COMMANDS ###################################
+
+freebayes --fasta-reference $reference_fasta --ploidy $ploidy --bam $alignments_bam > $output_vcf
+
+################################################################################
+<<CITATIONS
+    - Acknowledge TAMU HPRC: https://hprc.tamu.edu/research/citations.html
+
+    - FreeBayes:
+        Garrison E, Marth G. Haplotype-based variant detection from short-read sequencing.
+        arXiv preprint arXiv:1207.3907 [q-bio.GN] 2012
+CITATIONS
